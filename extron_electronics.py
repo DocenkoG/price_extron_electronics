4b9ac33f2669c9a7bfbf580e210db28c20f4dbfb
DocenkoG <docn@mail.ru>
2022-09-12 14:00:20 +0300

    Убрал from selenium... import лишние объекты, которые добавлял в процессе поиска команд для webdriver.

--- extron_electronics.py
+++ extron_electronics.py
@@ -156,8 +156,6 @@ def convert_excel2csv(cfg):
 
 def download( cfg ):
     from selenium import webdriver
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.common.by import By
     from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.remote.remote_connection import LOGGER
